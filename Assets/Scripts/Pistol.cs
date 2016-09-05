91b808d4c1df49026836351929bde77187542098
Tanner Hartwig <tanner.p.hartwig47@gmail.com>
2016-09-04 19:04:26 -0500

    Useable objects, guns now fire projectiles.

---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using VRTK;
+using System.Collections;
+
+public class Pistol : VRTK_InteractableObject
+{
+    // Handling
+    public float bulletSpeed = 1000f;
+    public float bulletLife = 5f;
+
+    // Component
+    private GameObject bullet;
+
+    public override void StartUsing(GameObject usingObject)
+    {
+        base.StartUsing(usingObject);
+        FireBullet();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        bullet = transform.Find("Bullet").gameObject;
+        bullet.SetActive(false);
+    }
+
+    private void FireBullet()
+    {
+        GameObject bulletClone = Instantiate(bullet, bullet.transform.position, bullet.transform.rotation) as GameObject;
+        bulletClone.SetActive(true);
+        Rigidbody rb = bulletClone.GetComponent<Rigidbody>();
+        rb.AddForce(bullet.transform.forward * bulletSpeed);
+        Destroy(bulletClone, bulletLife);
+    }
+}
